2933fa0833037cdf41443befbe53a3abacb662fd
demonicirfan <hashtag.irfan@gmail.com>
2021-08-03 16:34:02 +0530

    ping pong

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-//const config = require("./config.json");
+const config = require("./config.json");
+const command = require("./command");
 
 client.on("ready", () => {
   console.log("the client is ready");
+
+  command(client, "ping", (message) => {
+    message.channel.send("pong!");
+  });
 });
 
 client.login(process.env.DJS_TOKEN);

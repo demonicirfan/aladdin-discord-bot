e569f5e846b3bbc86ca436f47972b3356e6d337c
demonicirfan <hashtag.irfan@gmail.com>
2021-08-24 12:27:21 +0530

    update

--- command.js
+++ command.js
@@ -1,19 +1,22 @@
 const { prefix } = require("./config.json");
 
 module.exports = (client, aliases, callback) => {
+  //aliases = ["ping", "test"]
   if (typeof aliases === "string") {
     aliases = [aliases];
-  }
+  } 
+  //alias is what are the commands
+  //message is what the user has sent with prefix
 
-  client.on("message", (message) => {
-    const { content } = message;
+  client.on("message", (message) => { //message = !adi
+    const { content } = message; // !adi  --> content = !adi
 
-    aliases.forEach((alias) => {
-      const command = `${prefix}${alias}`;
+    aliases.forEach((alias) => { //for each element in alias it will check eg ping and test
+      const command = `${prefix}${alias}`; //1. command = !ping
 
-      if (constent.startWith(`${command}`) || content === command) {
+      if (content.startsWith(`${command} `) || content === command) {
         console.log(`Running the command ${command}`);
-        callback(message)
+        callback(message, alias);
       }
     });
   });
--- index.js
+++ index.js
@@ -5,11 +5,29 @@ const config = require("./config.json");
 const command = require("./command");
 
 client.on("ready", () => {
-  console.log("the client is ready");
+  console.log("The client is ready!");
 
-  command(client, "ping", (message) => {
-    message.channel.send("pong!");
+  command(client, ["ping", "test"], (message, alias) => {
+    message.channel.send(`${alias} chutya ha`);
+  });
+
+  command(client, "servers", (message) => {
+    client.guilds.cache.forEach((guild) => {
+      console.log(guild);
+      message.channel.send(`${guild.name} has a total of ${guild.memberCount}`);
+    });
+  });
+
+  command(client, ["cc", "clearchannel"], (message) => {
+    if (message.member.hasPermission("ADMINISTRATOR")) {
+      message.channel.messages.fetch().then((result) => {
+        console.log(results);
+        message.channel.bulkDelete(results);
+      });
+    }
   });
 });
 
-client.login(process.env.DJS_TOKEN);
+client.login(config.token);
+
+// wifi password => irfanwifi123
